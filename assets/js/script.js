32d36ea8a3b037d1d54c8e0c6edb36cb436837cc
OnlyMeHere <bennett45.j.m@gmail.com>
2022-06-23 11:49:03 -0700

    add phsudocode

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+
+// ---Display a button labeled Play---//
+
+
+
+// ---When button is clicked the game begins--//
+
+
+// ---- Prompt appears to ask for first move -- //
+
+
+//---user selects either Rock Paper or Scissors---//
+
+
+// --If any other charictors are entered then message --//
+//-- Please enter only one: Rock Paper or Scissors --//
+
+
+// --- assign user responce to variable--- //
+
+
+// --- generate random choice for comuter responce and asign to a variable ---//
+
+// ---compare user variable to computer random variable and display answer --//
+
+// --- alerts user of result ---//
