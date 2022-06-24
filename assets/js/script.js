f0fc47b264ef424a9358615717d6d3980f536297
Corasinth <beforeyouknowkindness@gmail.com>
2022-06-24 00:53:38 -0500

    add basic game functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,15 +26,62 @@
 
 // --- alerts user of result ---//
 
-window.confirm("Want to play Rock Paper Scissors? \n OK or Cancel");
+var computerChoices = ["R", "P", "S"]
 
-if (window.confirm("Want to play Rock Paper Scissors? \n OK or Cancel") === true) {
-    var userChoice = window.prompt ("Please select R, P, or S")    
+const getComputerChoice = computerChoices[Math.floor(Math.random(0, 3) * computerChoices.length)];
+console.log (getComputerChoice)
+
+function mainFunction () {
+    var userChoice = window.prompt ("Please choose rock (R), paper (P), or scissors (S)")    
         if (userChoice === "R") {
-            console.log ("R")
-        } else if (userChoice === "P") {
-            console.log ("P")
+            //console.log ("R")
+            switch (getComputerChoice) {
+                case ("R"):
+                window.alert ("The Computer chose Rock. It's a tie!")
+                break;
+                case ("P"):
+                window.alert ("The Computer chose Paper. You Lose!")
+                break;
+                case ("S"):
+                window.alert ("The Computer chose Scissors. You Win!")
+                break;
+            }
+         } else if (userChoice === "P") {
+            //console.log ("P")
+            switch (getComputerChoice) {
+                case ("R"):
+                window.alert ("The Computer chose Rock. You Win!" )
+                break;
+                case ("P"):
+                window.alert ("The Computer chose Paper. It's a tie!")
+                break;
+                case ("S"):
+                window.alert ("The Computer chose Scissors. You Lose!")
+                break;
+            }
         } else if (userChoice === "S") {
-            console.log ("S")
+            //console.log ("S")
+            switch (getComputerChoice) {
+                case ("R"):
+                window.alert ("The Computer chose Rock. You Lose!" )
+                break;
+                case ("P"):
+                window.alert ("The Computer chose Paper. You Win!")
+                break;
+                case ("S"):
+                window.alert ("The Computer chose Scissors. It's a tie!")
+                break;
+            }
+        } else if (Boolean(userChoice) === true) {
+                mainFunction ()
+        } else {
+            return;
         }
-}
+    }
+
+(function initalPrompt () {
+window.confirm("Want to play Rock Paper Scissors? \n OK or Cancel");
+    if (window.confirm("Want to play Rock Paper Scissors? \n OK or Cancel") === true) {
+        mainFunction ()
+    }
+})()
